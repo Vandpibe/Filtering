b859d0f03c863637750c9e3113dcf61a3ed4e5f6
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-12-03 10:05:06 +0100

    Initial

---
+++ src/Vandpibe/Filter/AutolinkFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Vandpibe\Filter;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @package Vandpibe
+ */
+class AutolinkFilter implements FilterInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function filter($value, array $context)
+    {
+        if (false == $context['auto_link']) {
+            return $value;
+        }
+
+        $searches = array(
+            '/(((f|ht){1}tp:\/\/)[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+)/',
+            '/([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\\+.~#?&\/\/=]+)/',
+            '/([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})/',
+        );
+
+        $replaces = array(
+            '<a href="\\1">\\1</a>',
+            '\\1<a href="http://\\2">\\2</a>',
+            '<a href="mailto:\\1">\\1</a>',
+        );
+
+        return preg_replace($searches, $replaces, $value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configure(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'auto_link' => true,
+        ));
+    }
+}
---
+++ src/Vandpibe/Filter/FilterInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Vandpibe\Filter;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @package Vandpibe
+ */
+interface FilterInterface
+{
+    /**
+     * @param string
+     * @param array $context
+     * @return string
+     */
+    public function filter($value, array $context);
+
+    /**
+     * Easy validation of the options needed in the context. This can also
+     * set default options that will be merged with the ones given in the context.
+     *
+     * @param OptionsResolverInterface $resolver
+     */
+    public function configure(OptionsResolverInterface $resolver);
+}
